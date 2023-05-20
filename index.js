f394fece958e4c0623ddf53e5c8b9d2bd26ad92f
Emily Yao <emilyyao9898_@hotmail.com>
2023-05-20 16:11:50 -0700

    check it cards are the same

---
+++ index.js
@@ -0,0 +1,29 @@
+const setup = () => {
+
+    let firstCard = undefined
+    let secondCard = undefined
+
+    $(".card").on(("click"), function () {
+      $(this).toggleClass("flip");
+      
+      if (!firstCard)
+      firstCard = $(this).find(".front_face")[0]
+    else {
+      secondCard = $(this).find(".front_face")[0]
+      console.log(firstCard, secondCard);
+      if (
+        firstCard.src
+        ==
+        secondCard.src
+      )
+        console.log("match")
+      else
+        console.log("no match")
+    }
+  });
+}
+
+   
+  
+  
+  $(document).ready(setup)
